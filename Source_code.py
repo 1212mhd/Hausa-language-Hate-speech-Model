5ba5844ba4b9fffa03d89611d411635dfdcd3f05
1212mhd <mm1469377@gmail.com>
2025-08-23 15:30:19 +0100

    Add new file
    Files

---
+++ Source_code.py
@@ -0,0 +1,57 @@
+# Load Data
+
+import pandas as pd
+data = pd.read_csv("labeled_data.csv")
+
+from nltk.util import pr
+import nltk
+nltk.download('stopwords')
+import pandas as pd
+import numpy as np
+import re
+
+stemmer = nltk.SnowballStemmer("english")
+from nltk.corpus import stopwords
+import string
+stopword=set(stopwords.words('english'))
+data = pd.read_csv("labeled_data.csv")
+
+data["labels"] = data["class"].map({0: "Hate Speech", 
+                                    1: "Offensive Language", 
+                                    2: "No Hate and Offensive"})
+data = data[["tweet", "labels"]]
+def clean(text):
+    text = str(text).lower()
+    text = re.sub('\\[.*?\\]', '', text)
+    text = re.sub('https?://\\S+|www\\.\\S+', '', text)
+    text = re.sub('<.*?>+', '', text)
+    text = re.sub('[%s]' % re.escape(string.punctuation), '', text)
+    text = re.sub('\\n', '', text)
+    text = re.sub('\\w*\\d\\w*', '', text)
+    text = [word for word in text.split(' ') if word not in stopword]
+    text=" ".join(text)
+    text = [stemmer.stem(word) for word in text.split(' ')]
+    text=" ".join(text)
+    return text
+data["tweet"] = data["tweet"].apply(clean)
+
+print(data.head())
+
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+
+x = np.array(data["tweet"])
+y = np.array(data["labels"])
+
+cv = CountVectorizer()
+X = cv.fit_transform(x) # Fit the Data
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
+
+clf = DecisionTreeClassifier()
+clf.fit(X_train,y_train)
+
+sample = "good morning "
+data = cv.transform([sample]).toarray()
+print(clf.predict(data))
+
